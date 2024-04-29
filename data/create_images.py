0eafe80fa5328fed73f00ef81aab2983ccbc62b6
vipulSharma18 <vipuls181999@gmail.com>
2024-04-29 19:30:27 -0400

    generate image doc

--- data/create_images.py
+++ data/create_images.py
@@ -1,8 +1,11 @@
 # z_s circle
 # z_c square 
+# use alpha_c and alpha_s to control size/pixel footprint for train & val. 
+# test is kept as the baseline footprint/base z values. 
+# The baseline footprint is 400px
 # color = respective feature values 
 # manipulate size of square and circle. 
 
 # random position of circle and square, no overlap
 # output image size is 224x224
-# predictivity p_s = p_c = 0.9
+
