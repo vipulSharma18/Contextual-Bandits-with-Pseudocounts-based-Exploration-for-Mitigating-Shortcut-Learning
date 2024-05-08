8810a940707c17eb35d83c56b4665bb338d77756
vipul_sharma@brown.edu <vsharm44@vscode1.oscar.ccv.brown.edu>
2024-05-07 23:47:51 -0400

    added raw bias data and utility to download wandb data

--- utility.py
+++ utility.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import numpy as np
+import wandb
+
+api = wandb.Api()
+
+run_id = "9svtovs6"
+run = api.run(f"vipul/RL_Project_CSCI2951F/{run_id}")
+history_df = run.history()
+history_df.to_csv("measure_bias/bias_data.csv", index=False)
