1e53cc664a467972cba25bd82c0efe55c261a9b3
vipul_sharma@brown.edu <vsharm44@gpu2001.oscar.ccv.brown.edu>
2024-05-09 00:50:31 -0400

    image reconstruction and query key bugfix

--- image_augmenter/contrastive_dataset.py
+++ image_augmenter/contrastive_dataset.py
@@ -10,10 +10,10 @@ import random
 from PIL import Image
 
 class PatchOperations(): 
-    def __init__(self, patch_size=56, image_size=224): 
+    def __init__(self, patch_size=56, image_size=(224,224)): 
         self.patch_size = patch_size
         self.image_size = image_size
-        self.num_patches = (image_size//patch_size)**2
+        self.num_patches = (image_size[0]//patch_size)**2
         self.augmentations = transforms.Compose([
             transforms.RandomChoice([
                 transforms.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10),
@@ -22,21 +22,30 @@ class PatchOperations():
                 transforms.RandomRotation(degrees=45),
             ])
         ])
-    def query_key(self, image):
+    def query_key(self, images):
         """"Query and Key returned as a list of augmented patches """
-        patches = self.extract_patches(image)
-        # Apply two different augmentations
-        results = []
-        for patch in patches: 
-            query = self.augmentations(patch)
-            key = self.augmentations(patch)
-            results.append((query, key))
-        return results
+        all_queries = []
+        all_keys = []
+        for image in images:
+            patches = self.extract_patches(image)
+            queries = []
+            keys = []
+            for patch in patches:
+                query = self.augmentations(patch)
+                key = self.augmentations(patch)
+                queries.append(query)
+                keys.append(key)
+            all_queries.extend(queries)
+            all_keys.extend(keys)
+        return all_queries, all_keys
     def extract_patches(self, image):
         """ Extract non-overlapping patches from an image. """
-        patches = [image.crop((i % 4 * self.patch_size, i // 4 * self.patch_size,
-                               (i % 4 + 1) * self.patch_size, (i // 4 + 1) * self.patch_size))
-                   for i in range(self.num_patches)]
+        #image channels * height * width
+        patches = []
+        for i in range(0, image.size(1), self.patch_size):  # Iterate over height
+            for j in range(0, image.size(2), self.patch_size):  # Iterate over width
+                patch = image[:, i:i + self.patch_size, j:j + self.patch_size]
+                patches.append(patch)
         return patches
     def reconstruct_image(self, patches, mask): 
         """Input patches and bandit's returned mask, recreate original image with cyan masks"""
@@ -44,13 +53,13 @@ class PatchOperations():
         idx = 0
         for i in range(0, self.image_size[0], self.patch_size):
             for j in range(0, self.image_size[1], self.patch_size):
-                patch = patches[idx]
+                
                 if idx in mask:
                     # Create a cyan patch for masking
-                    blue_patch = Image.new('RGB', (self.patch_size, self.patch_size), (0, 255, 255))
-                    image.paste(blue_patch, (i, j))
+                    patch = Image.new('RGB', (self.patch_size, self.patch_size), (0, 255, 255))
                 else:
-                    image.paste(patch, (i, j))
+                    patch = transforms.ToPIL()(patches[idx])
+                image.paste(patch, (i, j))
                 idx += 1
-        return image
+        return transforms.ToTensor()(image)
     
