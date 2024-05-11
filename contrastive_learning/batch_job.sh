90fdeea95b8125fcc669903c586bcc09b9504bd7
vipul <vipul_sharma@brown.edu>
2024-05-10 23:47:53 -0400

    push push

--- contrastive_learning/batch_job.sh
+++ contrastive_learning/batch_job.sh
@@ -2,26 +2,11 @@
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH --mem=10G
-#SBATCH -t 0:45:00
-#SBATCH -J 9_5
-#SBATCH -o 9_5.out
-#SBATCH -e 9_5.out
+#SBATCH -t 1:30:00
+#SBATCH -J 6_5
+#SBATCH -o 6_5.out
+#SBATCH -e 6_5.out
 #SBATCH -p gpu --gres=gpu:1 --gres-flags=enforce-binding
-
-##module load anaconda
-##source /users/anagara8/data/anagara8/mae/bin/activate
-##conda activate /users/akarkada/data/akarkada/conda/envs/py39
-
-#module load python/3.9.0
-#module load cuda/11.7.1
-#module load gcc/10.2
-#module load libjpeg-turbo/2.0.2 
-#module load opencv/3.4.1
-#. /gpfs/runtime/opt/anaconda/2022.05/etc/profile.d/conda.sh
-#. /gpfs/runtime/opt/anaconda/2022.05/etc/profile.d/conda.sh
-#. /gpfs/runtime/opt/anaconda/2022.05/etc/profile.d/conda.sh
-#conda activate
-#. /gpfs/runtime/opt/anaconda/2022.05/etc/profile.d/conda.sh
 
 if [ -f /etc/bashrc ]; then
         . /etc/bashrc
@@ -47,4 +32,4 @@ unset __conda_setup
 module load cuda
 conda activate dl_project
 
-python -u train_model.py 0.9_5
+python -u train_model.py 0.6_5
